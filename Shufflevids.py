3741d3e7542b43c98e413466f8c800723007aacf
tswart-res <83353523+tswart-res@users.noreply.github.com>
2021-04-29 10:59:38 +0100

    Add files via upload

--- Shufflevids.py
+++ Shufflevids.py
@@ -26,23 +26,19 @@ for index, a in enumerate(asps):
   
 random.shuffle(asps)    
 
-print(asps)
+#print(asps)
 randomordertxt = '{}\{}_RandomisedOrder.txt'.format(root, rootname)
 randomorderVid = '{}\{}_RandomisedOrder.{}'.format(root, rootname,videoextension)
 
 with open(randomordertxt, 'w') as file_handler:
     for item in asps:
         file_handler.write("{}\n".format(item))
-        
-# C:\Users\liltimmy\Documents\Python\P002>ffmpeg -f concat -i P000_2.txt -c copy P000_2_Randomized.mp4
-        
-# ffmpeg -i "P000_1.mp4" -vcodec msmpeg4v3 -b:v 4000K -acodec wmav2 -b:a 192K -aspect 4:3 -s 1280x960 -ss 00:00:00 -to 00:10:00 "Part 1 Vid 1.wmv"
-
-# ffmpeg -i "P002_1_Randomized.mp4" -vcodec msmpeg4v3 -b:v 4000K -acodec wmav2 -b:a 192K -aspect 4:3 -s 1280x960 -ss 00:00:00 -to 00:10:00 "Part 1 Vid 2.wmv"
-        
-# ffmpeg -i "P000_2.mp4" -vcodec msmpeg4v3 -b:v 4000K -acodec wmav2 -b:a 192K -aspect 4:3 -s 1280x960 -ss 00:00:00 -to 00:10:00 "Part 2 Vid 1.wmv"
-
-# ffmpeg -i "P000_2_Randomized.mp4" -vcodec msmpeg4v3 -b:v 4000K -acodec wmav2 -b:a 192K -aspect 4:3 -s 1280x960 -ss 00:00:00 -to 00:10:00 "Part 2 Vid 2.wmv"
+              
 concat_text = "ffmpeg -f concat -safe 0 -i {} -c copy {}".format(randomordertxt, randomorderVid)
 
 subprocess.check_output(concat_text, shell=True)
+
+
+## if you then want to convert the video to a different format or strip the audio from the video you can also use ffmpeg with different arguments eg.,
+
+# ffmpeg -i "P000_1.mp4" -vcodec msmpeg4v3 -b:v 4000K -acodec wmav2 -b:a 192K -aspect 4:3 -s 1280x960 -ss 00:00:00 -to 00:10:00 "Part 1 Vid 1.wmv"
